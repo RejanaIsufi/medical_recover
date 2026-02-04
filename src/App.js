9fecc8c86459fea05e5c34dee09e1e2d1503ce11
Rejana Isufi <150076726+RejanaIsufi@users.noreply.github.com>
2026-02-04 22:27:18 +0100

    footer complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Navbar from './components/organisms/navbar/Navbar';
+import Footer from './components/organisms/footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
 
     </div>
